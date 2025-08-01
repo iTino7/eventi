6d0059acffc5929dc8bb27fde1bebb2574d7664d
iTino7 <Sabatino.b007@outlook.it>
2025-08-01 12:05:35 +0200

    Create payloads

---
+++ src/main/java/tinoborrelli/eventi/payloads/EventDTO.java
@@ -0,0 +1,23 @@
+package tinoborrelli.eventi.payloads;
+
+import jakarta.validation.constraints.*;
+
+public record EventDTO(
+        @NotBlank(message = "Name is required")
+        @Size(min = 3, message = "Name must be at least 3 characters long")
+        String name,
+        @NotBlank(message = "Description is required")
+        @Size(min = 3, message = "Description must be at least 3 characters long")
+        String description,
+        @NotBlank(message = "Location is required")
+        @Size(min = 3, message = "Location must be at least 3 characters long")
+        String location,
+        @NotNull(message = "Date is required")
+        @FutureOrPresent
+        String date,
+        @NotNull(message = "Available tickets is required")
+        @Min(1)
+        @Positive
+        int availableTickets
+) {
+}
---
+++ src/main/java/tinoborrelli/eventi/payloads/UserDTO.java
@@ -0,0 +1,28 @@
+package tinoborrelli.eventi.payloads;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record UserDTO(
+        @NotBlank(message = "Username is required")
+        @Size(min = 3, message = "Username must be at least 3 characters long")
+        String username,
+        @NotBlank(message = "Email is required")
+        @Email(message = "Invalid email")
+        String email,
+        @NotBlank(message = "Password is required")
+        @Size(min = 15, message = "Password must be at least 15 characters long")
+        String password,
+        @NotBlank(message = "First name is required")
+        @Size(min = 3, message = "First name must be at least 3 characters long")
+        String firstName,
+        @NotBlank(message = "Last name is required")
+        @Size(min = 3, message = "Last name must be at least 3 characters long")
+        String lastName,
+        @NotBlank(message = "Role is required")
+        @Pattern(regexp = "USER|ORGANIZER", message = "Role must be either USER or EVENT_MANAGER")
+        String role) {
+}
